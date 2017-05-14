9d23078f81beb3361e1469341e97ed11439ddd08
lishuhuakai <lishuhuakai@gmail.com>
2017-05-14 09:54:31 +0800

    添加了一些说明!

--- checksum.c
+++ checksum.c
@@ -29,7 +29,7 @@ sum_every_16bits(void *addr, int count)
 	return sum;
 }
 
-// checksum 用于计算校验值
+/* checksum 用于计算校验值 */
 uint16_t 
 checksum(void *addr, int count, int start_sum)
 {
@@ -45,14 +45,16 @@ int
 tcp_udp_checksum(uint32_t saddr, uint32_t daddr, uint8_t proto,
 	uint8_t *data, uint16_t len)
 {
+	/* we need to ensure that saddr and daadr are in netowrk byte order */
 	uint32_t sum = 0;
 	struct pseudo_head head;
 	memset(&head, 0, sizeof(struct pseudo_head));
-	// 需要保证传入的daddr以及saddr是网络字节序
+	/* 需要保证传入的daddr以及saddr是网络字节序 */
 	head.daddr = daddr;
 	head.saddr = saddr;
-	// 对于TCP来说,proto = 0x06,而对于UDP来说proto = 0x17
-	head.proto = proto; // 对于只有1个字节的数据,不需要转换字节序
+	/* 对于TCP来说,proto = 0x06,而对于UDP来说proto = 0x17 */
+	head.proto = proto; /* sizeof(proto) == 1,  
+						对于只有1个字节的数据,不需要转换字节序 */
 	head.len = htons(len);
 	sum = sum_every_16bits(&head, sizeof(struct pseudo_head));
 	return checksum(data, len, sum);
